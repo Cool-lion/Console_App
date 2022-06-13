32af6d46484f5a3d5a445f2de8318595f8fca541
Cool-lion <85652008+Cool-lion@users.noreply.github.com>
2022-06-13 08:58:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var First_name = "Jeff ";
+console.log(First_name);
+var Last_name ="Bezos";
+console.log(Last_name);
+var Whole_name =First_name.concat(Last_name);
+console.log(Whole_name);
+var a = 10;
+var b = 5;
+var multiplication = Math.abs(10 * 5);
+console.log(multiplication);
+var addition = Math.abs(10 + 5);
+console.log(addition);
+var subtraction = Math.abs(10 - 5);
+console.log(subtraction);
+var division = Math.abs(10 / 5);
+console.log(division);
+
+
+
+
+
+
+
